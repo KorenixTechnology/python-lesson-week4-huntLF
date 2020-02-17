f92ed2d0b851c7148983dce681eddb2419e63f66
Paul <magicryze@gmail.com>
2020-02-17 17:30:03 +0800

    Update hw_week4.py

--- hw_week4.py
+++ hw_week4.py
@@ -1,6 +1,12 @@
 def hw_week4(name, age):
-    # Your code here
-    pass
+    if name == 'Alice':
+        print('Hi, Alice.')
+    elif age < 12:
+        print('You are not Alice, kiddo.')
+    elif age > 2000:
+        print('Unlike you, Alice is not an undead, immortal vampire.')
+    elif age > 100:
+        print('You are not Alice, grannie.')
 
 
 if __name__ == '__main__':
